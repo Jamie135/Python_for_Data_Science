d715480fa2a934ffa7e001868122ac8ec5af3016
Jooma <jamie.gworld@hotmail.com>
2024-07-27 12:33:19 +0200

    ex02

---
+++ Python_0_Starting/ex01/format_ft_time.py
@@ -0,0 +1,9 @@
+import time as t
+import datetime as dt
+
+
+current_time = f"Seconds since January 1, 1970: {t.time():,.4f} or {t.time():.2e} in scientific notation"
+current_date = f"{dt.datetime.today():%b %d %Y}"
+
+print(current_time)
+print(current_date)
