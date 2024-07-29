346d6bda10b155bf097acf470e8555bc7f26241b
Jooma <jamie.gworld@hotmail.com>
2024-07-29 17:49:03 +0200

    module1/ex03

---
+++ Python_1_Array/ex02/tester.py
@@ -0,0 +1,3 @@
+from load_image import ft_load
+
+print(ft_load("../resources/landscape.jpg"))
---
+++ Python_1_Array/ex03/zoom.py
@@ -0,0 +1,6 @@
+import os  # for path check
+import array  # for flake8 norm
+import numpy as np
+from PIL import Image
+
+
