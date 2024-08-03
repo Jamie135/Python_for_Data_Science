0f66bbcbdb27e50d040343256e65b4cdbc3a44f4
Jooma <jamie.gworld@hotmail.com>
2024-08-03 18:16:27 +0200

    module3/ex03

--- Python_3_OOP/ex02/DiamondTrap.py
+++ Python_3_OOP/ex02/DiamondTrap.py
@@ -2,4 +2,25 @@ from S1E7 import Baratheon, Lannister
 
 
 class King(Baratheon, Lannister):
-    """"""
+    """Representing the King"""
+
+    def __init__(self, first_name, is_alive=True):
+        """Constructor for then King
+        inherited from Baratheon and Lannister"""
+        super().__init__(first_name, is_alive)
+
+    def set_eyes(self, eyes) -> str:
+        """set eye's color"""
+        self.eyes = eyes
+
+    def set_hairs(self, hairs) -> str:
+        """set hair's color"""
+        self.hairs = hairs
+
+    def get_eyes(self) -> str:
+        """return eye's color"""
+        return self.eyes
+
+    def get_hairs(self) -> str:
+        """return hair's color"""
+        return self.hairs
