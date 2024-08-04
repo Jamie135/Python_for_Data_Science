6d009ae17ad47e2608748159e1573fb5438c947f
Jooma <jamie.gworld@hotmail.com>
2024-08-04 18:48:57 +0200

    module4/ex02

---
+++ Python_4_Dod/ex01/in_out.py
@@ -0,0 +1,42 @@
+def square(x: int | float) -> int | float:
+    """return the square value"""
+    return x * x
+
+
+def pow(x: int | float) -> int | float:
+    """return the exponential value of the argument by itself"""
+    return x ** x
+
+
+def outer(x: int | float, function) -> object:
+    """
+    return an inner function that applies the given function to x,
+    updates x and increments a count each time it is called.
+    """
+    if not isinstance(x, (int, float)):
+        def error_func():
+            print("The first argument must be an int or float.")
+        return error_func
+    if not callable(function):
+        def error_func():
+            print("The second argument must be a callable function.")
+        return error_func
+
+    count = 0
+
+    def inner() -> float:
+        """
+        apply the given function to x, updates x with the result,
+        increments the count, and returns the new result.
+        """
+        # use nonlocal to modify outer scope variables
+        nonlocal count, x
+        if count == 0:
+            result = function(x)
+        else:
+            result = function(x)
+        x = result  # Update x with the new result
+        count += 1  # Increment the count
+        return result
+
+    return inner
---
+++ Python_4_Dod/ex01/tester.py
@@ -0,0 +1,35 @@
+from in_out import outer
+from in_out import square
+from in_out import pow
+
+my_counter = outer(3, square)
+print(my_counter())
+print(my_counter())
+print(my_counter())
+print("---")
+another_counter = outer(1.5, pow)
+print(another_counter())
+print(another_counter())
+print(another_counter())
+
+print("\n-----ERROR CASES----\n")
+
+my_counter = outer("3", square)
+print(my_counter())
+print(my_counter())
+print(my_counter())
+print("---")
+another_counter = outer(square, 3)
+print(another_counter())
+print(another_counter())
+print(another_counter())
+print("---")
+my_counter = outer("3", square)
+print(my_counter())
+print(my_counter())
+print(my_counter())
+print("---")
+another_counter = outer(3, "square")
+print(another_counter())
+print(another_counter())
+print(another_counter())
