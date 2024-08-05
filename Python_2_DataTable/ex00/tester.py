2b083b285b45c8186e27dce93b28d8e12ff221ef
Jamie135 <pbureera@paul-f4Br3s3.clusters.42paris.fr>
2024-08-05 11:16:57 +0200

    module2 done

--- Python_2_DataTable/ex00/tester.py
+++ Python_2_DataTable/ex00/tester.py
@@ -3,4 +3,4 @@ from load_csv import load
 
 print(load("../resources/life_expectancy_years.csv"))
 print(load("../resources/life_expectancy_years.c"))
-print(load("../resources/error.txt"))
+# print(load("../resources/error.txt"))
