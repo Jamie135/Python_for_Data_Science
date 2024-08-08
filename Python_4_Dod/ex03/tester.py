51ca9b28153a354251833403d82cb4916b6d0031
Jamie135 <pbureera@bess-f1r5s7.clusters.42paris.fr>
2024-08-08 12:00:12 +0200

    module4 done

--- Python_4_Dod/ex03/tester.py
+++ Python_4_Dod/ex03/tester.py
@@ -1,8 +1,12 @@
+import inspect
+from pprint import pprint
 from new_student import Student
 
 
 student = Student(name="Edward", surname="agle")
 print(student)
+print("\n------------------\n")
+pprint(inspect.getmembers(Student, inspect.isfunction))
 print("\n------------------\n")
 student = Student(name="Edward", surname="agle", id="toto")
 print(student)
